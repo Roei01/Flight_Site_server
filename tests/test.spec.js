1afd08cfce87c19daa3cb6144c98558cb03831ed
Roei01 <107935185+Roei01@users.noreply.github.com>
2024-12-03 16:48:22 +0200

    update

--- tests/test.spec.js
+++ tests/test.spec.js
@@ -4,7 +4,7 @@ describe('Stam test suit', () => {
   describe('test Array get sorted', () => {
     it('should return sorted array', () => {
       const names = ['adiel', 'yosi', 'ron'];
-      expect(names.sort()).to.be.eql(['ron', 'adiel', 'yosi']);
+      expect(names.sort()).to.be.eql(['adiel', 'ron', 'yosi']);
     });
   });
 });
